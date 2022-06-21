20291e30bcd0785d6b564152890d8f19e2f43188
Mikey <michaelangelomotta@gmail.com>
2022-06-21 16:13:31 -0400

    deployment again

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-const path = require('path');
-const express = require('express');
-const connectDB = require('./config/db');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
+const path = require("path");
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 // Connect Database
 connectDB();
 
-const logs = require('./routes/logs');
-const techs = require('./routes/techs');
+const logs = require("./routes/logs");
+const techs = require("./routes/techs");
 
 const app = express();
 
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
 }
 
-app.use('/api/logs', logs);
-app.use('/api/techs', techs);
+app.use("/api/logs", logs);
+app.use("/api/techs", techs);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+	);
 }
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
