c8bbcea552c84d3654c9aa75ed7ae1e678e8eebd
Mikey <michaelangelomotta@gmail.com>
2022-06-21 21:00:52 -0400

    deploy

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ app.use("/api/logs", logs);
 app.use("/api/techs", techs);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../client/build")));
+	app.use(express.static(path.join(__dirname, "/client/build")));
 
 	app.get("*", (_, res) => {
-		res.sendFile(path.join(__dirname, "../client/build/index.html"));
+		res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
 	});
 }
 
